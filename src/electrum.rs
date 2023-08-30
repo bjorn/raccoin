5b1fa28766bb53a77040063ba8b998f13c908114
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2023-08-30 16:54:52 +0200

    Take fee into account when converting Electrum to CTC

--- src/electrum.rs
+++ src/electrum.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use chrono::{NaiveDateTime, TimeZone};
 use chrono_tz::Europe::Berlin;
 use serde::Deserialize;
@@ -11,13 +13,13 @@ struct ElectrumHistoryItem {
     // confirmations: u64,
     value: f64,
     // fiat_value: f64,
-    // fee: Option<f64>,
+    fee: Option<f64>,
     // fiat_fee: Option<f64>,
     #[serde(deserialize_with = "deserialize_date_time")]
     timestamp: NaiveDateTime,
 }
 
-pub(crate) fn convert_electrum_to_ctc(input_path: &str, output_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub(crate) fn convert_electrum_to_ctc(input_path: &str, output_path: &str) -> Result<(), Box<dyn Error>> {
     println!("Converting {} to {}", input_path, output_path);
     let mut rdr = csv::ReaderBuilder::new()
         .from_path(input_path)?;
@@ -31,11 +33,14 @@ pub(crate) fn convert_electrum_to_ctc(input_path: &str, output_path: &str) -> Re
         wtr.serialize(CtcTx {
             id: Some(&record.transaction_hash),
             description: Some(&record.label),
+            fee_amount: record.fee,
+            fee_currency: if record.fee.is_none() { None } else { Some("BTC") },
+            blockchain: Some("BTC"),
             ..CtcTx::new(
                 utc_time,
                 if record.value < 0.0 { CtcTxType::Send } else { CtcTxType::Receive },
                 "BTC",
-                record.value.abs())
+                record.value.abs() - record.fee.unwrap_or(0.0))
         })?;
     }
 
