f5d68b35c7d13efcb2e26e58431446bfbbf7fe3f
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2023-09-30 20:45:06 +0200

    Explicitly choose dark style variant
    Due to some hardcoded colors, the UI currently doesn't fit light mode.

--- cryptotax_ui/build.rs
+++ cryptotax_ui/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let config =
         slint_build::CompilerConfiguration::new()
-        .with_style("fluent".into());
+        .with_style("fluent-dark".into());
     slint_build::compile_with_config("ui/appwindow.slint", config).unwrap();
 }
