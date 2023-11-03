cfd7fdc32713a9db8cdcc2810d985b562b9375cb
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2023-11-03 08:13:19 +0100

    Include version info in Windows exe
    Updated windows_exe_info to 0.4 and use its new feature to link version
    info from the cargo env.

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ fn main() {
     use std::path::Path;
 
     windows_exe_info::icon::icon_ico(Path::new("docs/favicon.ico"));
+    windows_exe_info::versioninfo::link_cargo_env();
 }
 
 #[cfg(target_family = "unix")]
